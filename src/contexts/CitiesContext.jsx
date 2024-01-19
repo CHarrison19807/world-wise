5d9b74fc2e360652f281be3c1d758b37b3096b82
Connor Harrison <connorh.uottawa@gmail.com>
2024-01-19 14:26:44 -0500

    Refactor citiescontext using the useReducer hook

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,4 +1,10 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useReducer,
+  useState,
+} from "react";
 
 let citiesArray = [
   {
@@ -40,29 +46,69 @@ let citiesArray = [
 ];
 const CitiesContext = createContext();
 const BASE_URL = "https://charrison19807.github.io/data/world-wise";
+const initialState = {
+  cities: [],
+  loading: false,
+  currentCity: {},
+  error: "",
+};
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "loading":
+      return { ...state, isLoading: true };
+    case "cities/loaded":
+      return { ...state, cities: action.payload, isLoading: false };
+    case "city/loaded":
+      return { ...state, currentCity: action.payload, isLoading: false };
+    case "city/created":
+      if (
+        !citiesArray.find((city) => city.cityName === action.payload.cityName)
+      )
+        citiesArray.push(action.payload);
+      return {
+        ...state,
+        isLoading: false,
+        cities: citiesArray,
+        currentCity: action.payload,
+      };
+    case "city/deleted":
+      citiesArray = citiesArray.filter((city) => city.id !== action.payload);
+      return {
+        ...state,
+        isLoading: false,
+        cities: citiesArray,
+        currentCity: {},
+      };
+    case "rejected":
+      return { ...state, isLoading: false, error: action.payload };
+    default:
+      throw new Error(`Unsupported action type: ${action.type}`);
+  }
+}
 function CitiesProvider({ children }) {
-  const [cities, setCities] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [currentCity, setCurrentCity] = useState({});
+  const [{ cities, loading, currentCity }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
   useEffect(function () {
     async function fetchCities() {
+      dispatch({ type: "loading" });
       try {
-        setLoading(true);
         const res = await fetch(`${BASE_URL}/cities.json`);
         const data = await res.json();
-        setCities(data.cities);
+        dispatch({ type: "cities/loaded", payload: data.cities });
       } catch (err) {
-      } finally {
-        setLoading(false);
+        dispatch({ type: "rejected", payload: "Error getting cities" });
       }
     }
     fetchCities();
   }, []);
 
   async function getCity(cityId) {
+    if (Number(cityId) === currentCity.id) return;
+    dispatch({ type: "loading" });
     try {
-      setLoading(true);
       let city;
       const res = await fetch(`${BASE_URL}/cities.json`);
       const data = await res.json();
@@ -70,17 +116,16 @@ function CitiesProvider({ children }) {
       if (!city) {
         city = citiesArray.find((city) => city.id === Number(cityId));
       }
-
-      setCurrentCity(city);
+      dispatch({ type: "city/loaded", payload: city });
     } catch (err) {
-    } finally {
-      setLoading(false);
+      dispatch({ type: "rejected", payload: "Error getting city" });
     }
   }
 
   async function createCity(newCity) {
+    dispatch({ type: "loading" });
+
     try {
-      setLoading(true);
       // CAN NOT POST TO GITHUB PAGES, UPDATE DATA LOCALLY
       // const res = await fetch(`${BASE_URL}/cities.json`, {
       //   method: "POST",
@@ -88,36 +133,36 @@ function CitiesProvider({ children }) {
       //   body: JSON.stringify(newCity),
       // });
       // const data = await res.json();
-      if (citiesArray.find((city) => city.cityName === newCity.cityName))
-        return;
-      citiesArray.push(newCity);
-      setCities(citiesArray);
+      dispatch({ type: "city/created", payload: newCity });
     } catch (err) {
-      alert("Error creating city");
-    } finally {
-      setLoading(false);
+      dispatch({ type: "rejected", payload: "Error creating city" });
     }
   }
 
   async function deleteCity(cityId) {
+    dispatch({ type: "loading" });
+
     try {
-      setLoading(true);
       // CAN NOT DELETE TO GITHUB PAGES, UPDATE DATA LOCALLY
       // const res = await fetch(`${BASE_URL}/cities.json/${id}`,
       //   { method: "DELETE" }
       // );
       // const data = await res.json();
-      citiesArray = citiesArray.filter((city) => city.id !== cityId);
-      setCities(citiesArray);
+      dispatch({ type: "city/deleted", payload: cityId });
     } catch (err) {
-      alert("Error deleting city");
-    } finally {
-      setLoading(false);
+      dispatch({ type: "rejected", payload: "Error deleting city" });
     }
   }
   return (
     <CitiesContext.Provider
-      value={{ cities, loading, currentCity, getCity, createCity, deleteCity }}
+      value={{
+        cities,
+        loading,
+        currentCity,
+        getCity,
+        createCity,
+        deleteCity,
+      }}
     >
       {children}
     </CitiesContext.Provider>
